d3f58f299d6239c91be68fe9dba5e1cb874dcc74
thalesleal <thalesvinicius.leal@gmail.com>
2025-09-10 15:18:42 -0300

    Refatoração de estrutura

--- TRAB 1/dist/routes/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.registerRoutes = registerRoutes;
-async function registerRoutes(app) {
-    app.get('/', async () => {
-        return { message: 'API protegida e rodando ✅' };
-    });
-}
